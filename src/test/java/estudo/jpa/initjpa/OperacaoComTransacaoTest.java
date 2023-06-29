d98407129ce4bf4f284866c39805654109eaf567
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-06-28 23:07:59 -0300

    03 - Teste com Transação

--- src/test/java/estudo/jpa/initjpa/OperacaoComTransacaoTest.java
+++ src/test/java/estudo/jpa/initjpa/OperacaoComTransacaoTest.java
@@ -1,2 +1,44 @@
-package estudo.jpa.initjpa;public class OperacaoComTransacaoTest {
+package estudo.jpa.initjpa;
+
+import estudo.jpa.EntityManagerTest;
+import estudo.jpa.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class OperacaoComTransacaoTest extends EntityManagerTest {
+
+    @Test
+    public void inserirOPrimeiroObjeto(){
+        Produto produto = new Produto();
+
+        produto.setId(2);
+        produto.setNome("Cavaleiro de ouro");
+        produto.setDescricao("Guardiões das doze casas");
+        produto.setPreco(new BigDecimal(8001));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+    }
+
+    @Test
+    public void abrirEFecharATrasacao(){
+
+        //Produto produto = new Produto();
+
+        entityManager.getTransaction().begin();
+
+//        entityManager.persist(produto);
+//        entityManager.merge(produto);
+//        entityManager.remove(produto);
+
+        entityManager.getTransaction().commit();
+    }
 }
