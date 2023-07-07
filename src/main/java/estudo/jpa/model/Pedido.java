5319c2a46f79237e72fb9099e6bb0ba0efee957d
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-07-06 23:56:12 -0300

    14 - Uso de objeto embutido com @Embeddable

--- src/main/java/estudo/jpa/model/Pedido.java
+++ src/main/java/estudo/jpa/model/Pedido.java
@@ -4,10 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -34,4 +31,7 @@ public class Pedido {
     private BigDecimal total;
 
     private StatusPedido status;
+
+    @Embedded
+    private EnderecoEntregaPedido enderecoEntrega;
 }
---
+++ src/test/java/estudo/jpa/mapeamentobasico/MapeamentoObjetoEmbutidoTest.java
@@ -0,0 +1,43 @@
+package estudo.jpa.mapeamentobasico;
+
+import estudo.jpa.EntityManagerTest;
+import estudo.jpa.model.EnderecoEntregaPedido;
+import estudo.jpa.model.Pedido;
+import estudo.jpa.model.StatusPedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class MapeamentoObjetoEmbutidoTest extends EntityManagerTest {
+
+    @Test
+    public void analisandoMapeamentoObjetoEmbutido(){
+        EnderecoEntregaPedido endereco = new EnderecoEntregaPedido();
+        endereco.setCep("00000-00");
+        endereco.setLogradouro("Rua dos testes");
+        endereco.setNumero("123");
+        endereco.setBairro("Centro");
+        endereco.setCidade("Testelândia");
+        endereco.setEstado("TEST");
+
+        Pedido pedido = new Pedido();
+        pedido.setId(1);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setTotal(new BigDecimal(250));
+        pedido.setEnderecoEntrega(endereco);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerifacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerifacao);
+        Assert.assertNotNull(pedidoVerifacao.getEnderecoEntrega());
+        Assert.assertNotNull(pedidoVerifacao.getEnderecoEntrega().getCep());
+    }
+}
