aedc8356fe6da5e2a077a92073f017323e4d4f74
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-07-12 23:56:20 -0300

    19 - Mapeamento autorelacionamentos

---
+++ src/test/java/estudo/jpa/relacionamentos/AutoRelacionamentoTest.java
@@ -0,0 +1,37 @@
+package estudo.jpa.relacionamentos;
+
+import estudo.jpa.EntityManagerTest;
+import estudo.jpa.model.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class AutoRelacionamentoTest extends EntityManagerTest {
+
+    @Test
+    public void VerificarRelacionamento(){
+        Categoria categoriaPai = new Categoria();
+        categoriaPai.setNome("Eletrônicos");
+
+        Categoria categoria = new Categoria();
+        categoria.setNome("Celular");
+        categoria.setCategoriaPai(categoriaPai);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoriaPai);
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificacao.getCategoriaPai());
+
+        Categoria categoriaPaiVerificacao = entityManager.find(Categoria.class, categoriaPai.getId());
+        Assert.assertFalse(categoriaPaiVerificacao.getCategorias().isEmpty());
+    }
+
+
+}
