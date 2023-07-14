f19735602774b9e2bdc53347efed76b82b5ffcb1
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-07-14 13:17:29 -0300

    20 - Removendo entidade referenciada

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -2,3 +2,7 @@ insert into produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Co
 insert into produto (id, nome, preco, descricao) values (3, 'Hyoga', 5800.0, 'Pó de diamante');
 
 insert into cliente (id, nome) values (1, 'RotaZ');
+
+insert into pedido(id, cliente_id, data_pedido, total, status) values (1, 1, sysdate(), 1000, 'AGUARDANDO');
+
+insert into item_Pedido(id, pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 1, 499.0, 2);
---
+++ src/test/java/estudo/jpa/relacionamentos/RemovendoEntidadesReferenciadasTest.java
@@ -0,0 +1,26 @@
+package estudo.jpa.relacionamentos;
+
+import estudo.jpa.EntityManagerTest;
+import estudo.jpa.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemovendoEntidadesReferenciadasTest extends EntityManagerTest {
+
+    @Test
+    public void removerEntidadeRelacionada() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+        entityManager.getTransaction().begin();
+        pedido.getItens().forEach(i -> entityManager.remove(i));
+        entityManager.remove(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, 1);
+        Assert.assertNull(pedidoVerificacao);
+    }
+}
