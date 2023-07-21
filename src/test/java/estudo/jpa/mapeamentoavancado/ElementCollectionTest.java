8c45f58dbd4526db2111f7399859030a0a3f8741
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-07-20 23:39:45 -0300

    30 - Uso de Collection de tipos basicos

---
+++ src/test/java/estudo/jpa/mapeamentoavancado/ElementCollectionTest.java
@@ -0,0 +1,26 @@
+package estudo.jpa.mapeamentoavancado;
+
+import estudo.jpa.EntityManagerTest;
+import estudo.jpa.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ElementCollectionTest extends EntityManagerTest {
+
+    @Test
+    public void applyTags() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setTags(Arrays.asList("ebook", "livro-digital"));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertFalse(produtoVerificacao.getTags().isEmpty());
+    }
+}
