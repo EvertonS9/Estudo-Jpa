e33d725e9c64ac18e1027b103b4677ad15773cd7
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-07-28 03:52:21 -0300

    42 - Criacao de tabela com UniqueConstraint e Index

---
+++ src/test/java/estudo/jpa/ddl/DDLTest.java
@@ -0,0 +1,12 @@
+package estudo.jpa.ddl;
+
+import estudo.jpa.EntityManagerTest;
+import org.junit.Test;
+
+public class DDLTest extends EntityManagerTest {
+
+    @Test
+    public void gerarDDL(){
+        //DDL...
+    }
+}
