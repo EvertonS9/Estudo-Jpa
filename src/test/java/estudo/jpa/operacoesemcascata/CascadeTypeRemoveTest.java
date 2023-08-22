4522f54ca24a34bef1f5ab94b1a6a3c31c960f6a
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-08-21 22:48:20 -0300

    58 - remocao em cascata de manytomany

--- src/test/java/estudo/jpa/operacoesemcascata/CascadeTypeRemoveTest.java
+++ src/test/java/estudo/jpa/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -4,10 +4,27 @@ import estudo.jpa.EntityManagerTest;
 import estudo.jpa.model.ItemPedido;
 import estudo.jpa.model.ItemPedidoId;
 import estudo.jpa.model.Pedido;
+import estudo.jpa.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
+
+    @Test
+    public void removerRelacaoProdutoCategoria() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assert.assertFalse(produto.getCategorias().isEmpty());
+
+        entityManager.getTransaction().begin();
+        produto.getCategorias().clear();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertTrue(produtoVerificacao.getCategorias().isEmpty());
+    }
 
     //@Test
     public void removerPedidoEItens() {
