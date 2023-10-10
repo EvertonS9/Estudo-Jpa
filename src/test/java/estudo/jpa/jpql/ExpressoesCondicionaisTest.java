2f9a41d667904310e3a115bcb90492b19ed16848
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-10-10 19:53:14 -0300

    74 - Exe maior e menor com datas / JPQL

--- src/test/java/estudo/jpa/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/estudo/jpa/jpql/ExpressoesCondicionaisTest.java
@@ -1,16 +1,28 @@
 package estudo.jpa.jpql;
 
 import estudo.jpa.EntityManagerTest;
+import estudo.jpa.model.Pedido;
 import estudo.jpa.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
 
+    @Test
+    public void usarMaiorMenorComDatas() {
+        String jpql = "select p from Pedido p where p.dataCriacao > :data";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        typedQuery.setParameter("data", LocalDateTime.now().minusDays(2));
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
     @Test
     public void usarMaiorMenor() {
         String jpql = "select p from Produto p " +
