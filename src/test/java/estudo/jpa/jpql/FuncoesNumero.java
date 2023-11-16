b961d25d55a0ea47d6e5a77c5c7dfb45066b0ce9
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-11-16 18:04:36 -0300

    83 - funcoes para numeros / JPQL

---
+++ src/test/java/estudo/jpa/jpql/FuncoesNumero.java
@@ -0,0 +1,25 @@
+package estudo.jpa.jpql;
+
+import estudo.jpa.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class FuncoesNumero extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoNumero(){
+
+        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p " +
+                " where abs(p.total) > 900";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+}
