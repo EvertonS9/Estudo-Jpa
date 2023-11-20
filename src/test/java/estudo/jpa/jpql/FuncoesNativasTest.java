754b8e11b4682da5bed2be146f76e657b392ea57
EvertonS9 <116914651+EvertonS9@users.noreply.github.com>
2023-11-20 16:13:07 -0300

    85 - funcoes nativas / JPQL

---
+++ src/test/java/estudo/jpa/jpql/FuncoesNativasTest.java
@@ -0,0 +1,27 @@
+package estudo.jpa.jpql;
+
+import estudo.jpa.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class FuncoesNativasTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoNativas() {
+        //Usando função criada
+        //String jpql = "select p from Pedido p where function('acima_media_faturamento', p.total) = 1";
+
+        String jpql = "select function('dayname', p.dataCriacao) from Pedido p " +
+                " where function('acima_media_faturamento', p.total) = 1";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+
+        List<String> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+}
